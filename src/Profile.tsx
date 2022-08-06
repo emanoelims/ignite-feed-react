787f44ea97fdc07e0e030032d2c7c135916eef56
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-05 19:05:07 -0700

    update profile to use avatar component

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,6 +1,7 @@
 import {PencilLine} from "phosphor-react";
 
 import styles from './Profile.module.css';
+import Avatar from "./Avatar";
 
 const Profile = () => {
   return (
@@ -9,7 +10,7 @@ const Profile = () => {
            src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80"
            alt=""/>
       <div className={styles.content}>
-        <img className={styles.avatar} src="https://github.com/emmanoeldev.png" alt="User avatar"/>
+        <Avatar />
         <h1 className={styles.name}>Emmanoel Mendes</h1>
         <p className={styles.role}>CEO @MendesAssessoria</p>
       </div>
