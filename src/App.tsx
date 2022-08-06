099b4e2f9a652364281d96d9381d147b5b0652e9
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-06 10:35:58 -0700

    change component to receive content by the parameter

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import Posts from "./Posts";
 import Profile from "./Profile";
 
 import styles from './App.module.css';
+import {useState} from "react";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "Emmanoel Mendes",
+    role: "CEO @MendesAssessoria",
+    userImage: "https://github.com/emmanoeldev.png",
+    coverImage: "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80"
+  });
   return (
     <>
       <Header/>
       <main className={styles.container}>
-        <Profile/>
+        <Profile
+          user={user}/>
         <Posts/>
       </main>
     </>
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -4,16 +4,25 @@ import Avatar from "./Avatar";
 
 import styles from './Profile.module.css';
 
-const Profile = () => {
+type ProfileProps = {
+  user: {
+    name: string,
+    role: string,
+    userImage: string,
+    coverImage: string
+  }
+};
+
+const Profile = ({user}: ProfileProps) => {
   return (
     <aside className={styles.container}>
       <img className={styles.cover}
-           src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80"
+           src={user.coverImage}
            alt=""/>
       <div className={styles.content}>
-        <Avatar image="https://github.com/emmanoeldev.png" />
-        <h1 className={styles.name}>Emmanoel Mendes</h1>
-        <p className={styles.role}>CEO @MendesAssessoria</p>
+        <Avatar image={user.userImage} />
+        <h1 className={styles.name}>{user.name}</h1>
+        <p className={styles.role}>{user.role}</p>
       </div>
       <footer className={styles.footer}>
         <button className={styles.button}><PencilLine width={20}/>Editar seu perfil</button>
