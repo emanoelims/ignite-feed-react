fb8109091820aaa93ed2aeeab408e43a6a25e2fa
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-05 17:40:31 -0700

    add profile section to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import Header from "./Header";
+import Profile from "./Profile";
+
+import styles from './App.module.css';
 
 const App = () => {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <main className={styles.container}>
+        <Profile/>
+      </main>
+    </>
   );
 }
 
