f8ba9c0a4e0856e7e7902c1e18a831ca9050c310
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2022-08-05 19:04:29 -0700

    extract avatar component

---
+++ src/Avatar.tsx
@@ -0,0 +1,9 @@
+import styles from "./Avatar.module.css";
+
+const Avatar = () => {
+  return (
+    <img className={styles.container} src="https://github.com/emmanoeldev.png" alt="User avatar"/>
+  );
+}
+
+export default Avatar;
